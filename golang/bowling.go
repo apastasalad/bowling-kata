dc87bee7d594d12d66a1c473c1950cccb11920ad
Craig Fothers <46976302+apastasalad@users.noreply.github.com>
2021-02-09 23:30:28 +1100

    Update bowling.go

--- golang/bowling.go
+++ golang/bowling.go
@@ -8,9 +8,10 @@ type Game struct {
 func (game *Game) score() int {
 	score := 0
 	counter := 0
+	
+	
 
 	for i := 0; i < frames; i++ {
-		score++
 		counter++
 	}
 
@@ -18,4 +19,5 @@ func (game *Game) score() int {
 }
 
 func (game *Game) roll(pins int) {
+	
 }
