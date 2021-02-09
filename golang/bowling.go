bc2e4e9e82c713042d3c89f75373d3176b00f721
Craig Fothers <46976302+apastasalad@users.noreply.github.com>
2021-02-09 22:50:42 +1100

    Create bowling.go

---
+++ golang/bowling.go
@@ -0,0 +1,20 @@
+package main
+
+const frames = 10
+
+type Game struct {
+}
+
+func (game *Game) score() int {
+	score := 0
+	counter := 0
+
+	for i := 0; i < frames; i++ {
+		counter++
+	}
+
+	return score
+}
+
+func (game *Game) roll(pins int) {
+}
