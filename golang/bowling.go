021de3195d5bd87b0aca305dc291b5a125d7c90a
Craig Fothers <46976302+apastasalad@users.noreply.github.com>
2021-02-10 14:40:45 +1100

    TestStrikeInFirstRoll

--- golang/bowling.go
+++ golang/bowling.go
@@ -1,11 +1,13 @@
 package main
 
+import "fmt"
+
 const frames = 10
 
 type BowlingGame struct {
 
 	// a game consists of 21 potential rolls.
-	// 10 frames; 2 rolls per frame and a final roll if there is a strike
+	// 10 frames; 2 rolls per frame and a final roll if there is a strike or a spare
 	rolls [21]int
 
 	// keep track of the current roll
@@ -14,23 +16,72 @@ type BowlingGame struct {
 
 // What's the score?
 func (game *BowlingGame) Score() int {
+	fmt.Printf("%v", game.rolls)
 	score := 0
-	counter := 0
+	frameCounter := 0
 
 	for i := 0; i < frames; i++ {
-		score += game.rolls[counter] + game.rolls[counter+1]
 
-		// fmt.Printf("the score is %v\n", score)
-		counter += 2
+		if DidWeRollAStrike(game, frameCounter) {
+			score += AwardAStrikeBonus(game, frameCounter)
+			fmt.Printf("%v, ", score)
+			frameCounter += 2
+		} else if DidWeRollASpare(game, frameCounter) {
+			score += AwardASpareBonus(game, frameCounter)
+			fmt.Printf("%v, ", score)
+			frameCounter += 2
+		} else {
+			score += game.rolls[frameCounter] + game.rolls[frameCounter+1]
+			fmt.Printf("%v, ", score)
+			frameCounter += 2
+		}
 	}
+
+	// count the last roll if it's awarded
+	score += game.rolls[20]
+
+	fmt.Printf("FINAL SCORE %v, ", score)
 
 	return score
 }
 
+// Look at the frame and determine if a spare was rolled
+func DidWeRollASpare(game *BowlingGame, frameIndex int) bool {
+	return game.rolls[frameIndex]+game.rolls[frameIndex+1] == 10
+}
+
+// Look at the frame and determine if a strike was rolled
+func DidWeRollAStrike(game *BowlingGame, frameIndex int) bool {
+	return game.rolls[frameIndex] == 10
+}
+
+// 10 points + the number of pins you knock down in the entire next frame.
+func AwardAStrikeBonus(game *BowlingGame, frameIndex int) int {
+	strikeBonus := 10
+	if frameIndex < 19 {
+		strikeBonus += game.rolls[frameIndex+2] + game.rolls[frameIndex+3]
+	}
+	return strikeBonus
+}
+
+// 10 points + the number of pins you knock down for your first attempt at the next frame.
+func AwardASpareBonus(game *BowlingGame, frameIndex int) int {
+	spareBonus := 10
+	if frameIndex < 19 {
+		spareBonus += game.rolls[frameIndex+2]
+	}
+	return spareBonus
+}
+
 // Knock down some pins
 func (game *BowlingGame) Roll(pins int) {
-
 	// Record the pins knocked down for this roll
+	// if we get a strike, move to the next frame
 	game.rolls[game.currentRolls] = pins
-	game.currentRolls++
+	if pins < 10 {
+		game.currentRolls++
+	} else {
+		game.currentRolls += 2
+	}
+
 }
--- golang/bowling_test.go
+++ golang/bowling_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 // A Gutter Game will always return a score of 0
 func TestGutterGame(t *testing.T) {
@@ -22,19 +24,39 @@ func TestOnePinAtATime(t *testing.T) {
 	}
 }
 
-// Spare in first roll, one pin down in each other roll, (score = 10 + 1 + 18 = 29)
+// Spare in first roll, one pin down in each other roll, (score = 11(spare) + 18 + 1 = 30)
 func TestSpareInFirstRoll(t *testing.T) {
 	game := BowlingGame{}
-	LetsRoll(&game, 1, 10)
-	LetsRoll(&game, 18, 1)
+	LetsRollASpare(&game)
+	LetsRoll(&game, 19, 1)
 
-	if game.Score() != 29 {
-		t.Errorf("Spare in first roll, one pin down in each other roll, got: %d, want: %d.", game.Score(), 29)
+	if game.Score() != 30 {
+		t.Errorf("Spare in first roll, one pin down in each other roll, got: %d, want: %d.", game.Score(), 30)
 	}
 }
 
-//
-//Spare in last roll, one pin down in each other roll, (score = 18 + 10 + 1 = 29)
+// Spare in last roll, one pin down in each other roll, (score = 18 + 10  = 28)
+func TestSpareInlastRoll(t *testing.T) {
+	game := BowlingGame{}
+	LetsRoll(&game, 18, 1)
+	LetsRollASpare(&game)
+
+	if game.Score() != 28 {
+		t.Errorf("Spare in last roll, one pin down in each other roll, got: %d, want: %d.", game.Score(), 28)
+	}
+}
+
+//Strike in first roll, one pin down in each other roll, (score = 12 + 19 = 31)
+func TestStrikeInFirstRoll(t *testing.T) {
+	game := BowlingGame{}
+	LetsRollAStrike(&game)
+	LetsRoll(&game, 19, 1)
+
+	if game.Score() != 31 {
+		t.Errorf("Strike in first roll, one pin down in each other roll, got: %d, want: %d.", game.Score(), 31)
+	}
+}
+
 //Strike in first roll, one pin down in each other roll, (score = 10 + 1 + 1 + 18 = 30)
 //Strike in last roll, one pin down in each other roll, (score = 18 + 10 + 1 + 1 = 30)
 //Golden game = all strikes (score = 300)
@@ -44,4 +66,15 @@ func LetsRoll(g *BowlingGame, numberOfRolls, pinsKnockedDown int) {
 	for i := 0; i < numberOfRolls; i++ {
 		g.Roll(pinsKnockedDown)
 	}
+}
+
+// Roll the ball and get a spare
+func LetsRollASpare(g *BowlingGame) {
+	g.Roll(8)
+	g.Roll(2)
+}
+
+// Roll the ball and get a strike
+func LetsRollAStrike(g *BowlingGame) {
+	g.Roll(10)
 }
