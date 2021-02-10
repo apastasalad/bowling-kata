63a200e27cdf603c7d655e1accf178a23a566f35
Craig Fothers <46976302+apastasalad@users.noreply.github.com>
2021-02-10 12:14:49 +1100

    Fix Gutter Game Add One Pin at a Time

--- golang/bowling.go
+++ golang/bowling.go
@@ -2,22 +2,35 @@ package main
 
 const frames = 10
 
-type Game struct {
+type BowlingGame struct {
+
+	// a game consists of 21 potential rolls.
+	// 10 frames; 2 rolls per frame and a final roll if there is a strike
+	rolls [21]int
+
+	// keep track of the current roll
+	currentRolls int
 }
 
-func (game *Game) score() int {
+// What's the score?
+func (game *BowlingGame) Score() int {
 	score := 0
 	counter := 0
-	
-	
 
 	for i := 0; i < frames; i++ {
-		counter++
+		score += game.rolls[counter] + game.rolls[counter+1]
+
+		// fmt.Printf("the score is %v\n", score)
+		counter += 2
 	}
 
 	return score
 }
 
-func (game *Game) roll(pins int) {
-	
+// Knock down some pins
+func (game *BowlingGame) Roll(pins int) {
+
+	// Record the pins knocked down for this roll
+	game.rolls[game.currentRolls] = pins
+	game.currentRolls++
 }
--- golang/bowling_test.go
+++ golang/bowling_test.go
@@ -4,10 +4,26 @@ import "testing"
 
 // A Gutter Game will always return a score of 0
 func TestGutterGame(t *testing.T) {
-	game := Game{}
-	game.roll(1)
+	game := BowlingGame{}
+	PlayAGame(&game, 20, 0)
 
-	if game.score() != 0 {
-		t.Errorf("The Gutter Game returned a non-zero score, got: %d, want: %d.", game.score(), 0)
+	if game.Score() != 0 {
+		t.Errorf("The Gutter Game returned a non-zero score, got: %d, want: %d.", game.Score(), 0)
+	}
+}
+
+// A Gutter Game will always return a score of 0
+func TestOnePinAtATime(t *testing.T) {
+	game := BowlingGame{}
+	PlayAGame(&game, 20, 1)
+
+	if game.Score() != 20 {
+		t.Errorf("The One Pin at a Time Game returned an incorrect, got: %d, want: %d.", game.Score(), 20)
+	}
+}
+
+func PlayAGame(g *BowlingGame, numberOfRolls, pinsKnockedDown int) {
+	for i := 0; i < numberOfRolls; i++ {
+		g.Roll(pinsKnockedDown)
 	}
 }
